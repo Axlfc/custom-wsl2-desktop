e25c95791e0bfc5104afc07ca6b202619a8dff09
Axel <axelfernandezcurros@gmail.com>
2021-11-01 10:00:09 +0100

    Update short.bat

--- Desktop/short.bat
+++ Desktop/short.bat
@@ -5,7 +5,7 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%SYSTEMDRIVE%\Users\Axel F C\Desktop\Files.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%SYSTEMDRIVE%\Users\Axel F C\Desktop\nemo.bat" >> %SCRIPT%
+echo oLink.TargetPath = "%SYSTEMDRIVE%\Users\Axel F C\Desktop\nemo.vbs" >> %SCRIPT%
 echo oLink.IconLocation = "%SYSTEMDRIVE%\Users\Axel F C\Desktop\Nemo_Desktop_icon.ico" >> %SCRIPT%
 echo oLink.WorkingDirectory = "%SYSTEMDRIVE%\Users\Axel F C\Desktop\Nemo_Desktop_icon.ico" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
